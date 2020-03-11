0f74acb03a3d63dc87f13e990cf1209a635aaa45
josefssonemil <emiljos@student.chalmers.se>
2020-03-11 10:44:24 +0100

    add gif showing how functionality should be

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 import * as foodChart from "./foodChart_new.js";
-import * as diabetesAgeChart from "./diabetesAgeChart.js";
 import * as diabetesGeneralChart from "./diabetesGeneral.js";
 
 const init = async () => {
   diabetesGeneralChart.init();
   foodChart.init();
-  diabetesAgeChart.init();
 };
 
 window.onload = init;
-
